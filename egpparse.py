d9fb27101aa7a74c75a1f7481fbe6745bfe0ed1b
Will Johnson <will@willj.co>
2016-02-09 18:27:55 -0600

    Adding initial version of egp-parse program.

---
+++ egpparse.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#f = open('query_test','r')
+#qry = f.readlines()
+#f.close()
+#qry="".join(qry)
+
+#from_line = re.compile('FROM(.+?)(?:WHERE|;)+?',flags=re.DOTALL).findall(qry)
+#table_list = re.compile('\w+\.\w+\s\w+',flags=re.DOTALL).findall(''.join(from_line))
+#select_line = re.compile('SELECT(.+?)FROM?',flags=re.DOTALL).findall(qry)
+#column_list = re.compile('\w+\.(?:\'(?:\w|\s|#)+\'n|\w+)',flags = re.DOTALL).findall(select_line[0])
+
+if __name__ == '__main__':
+    import xmltodict
+    import sys
+    import re
+    import os
+    import shutil
+    import time
+    import egpmove
+    import egpcols
+    import operator
+
+    startdir = '/home/will/pyscripts'
+    outdir = '/home/will/pyscripts/temp'
+    file_list =  egpmove.find_egps(os.getcwd())
+    temp_zip_list = []
+    with open('list_of_programs.txt','w') as fout:
+        for (srcdir, egp) in file_list:
+            egpmove.move_zip_egp(egp, srcdir, outdir)
+            fout.write('%s\t%s\n' % (srcdir, egp))
+    #Crawl the outdir to map new names of programs
+    new_zip_list = os.listdir(outdir)
+    #For every EGP, unzip it and extract project.xml file
+    for file in new_zip_list:
+        if file[-4:]==".zip":
+            egpmove.unpack_egp(outdir,file,outdir)
+        else:
+            pass
+    #Identify list of XML documents
+    xml_list_full = os.listdir(outdir)
+    xml_list = [file for file in xml_list_full if file[-4:]==".xml"]
+    for program in xml_list:
+        with open(outdir+'/'+program,'r') as egp:
+            xmldict = xmltodict.parse(egp.read())
+            print program
+            print "====================="
+            for query in egpcols.reach_task_code(xmldict):
+                col_list = egpcols.list_columns(query)
+                with open('column_list.txt','a') as coltxt:
+                    for col in col_list:
+                        coltxt.write('%s\t%s\n' % (program, col))
